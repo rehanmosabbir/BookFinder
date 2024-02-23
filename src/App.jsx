623f945592c5be15ec1800c1f72637b3ddc6f3fa
rehanmosabbir <rehanmosabbir@gmail.com>
2024-02-23 09:58:08 +0600

    add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
